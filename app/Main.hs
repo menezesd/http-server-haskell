15af665c281874f0a350a5113e93e4f796bffed3
Dean Menezes <dean.menezes@fortanix.com>
2023-10-19 09:32:51 -0500

    listen on port

--- app/Main.hs
+++ app/Main.hs
@@ -10,11 +10,8 @@ main = do
     -- You can use print statements as follows for debugging, they'll be visible when running tests.
     BLC.putStrLn "Logs from your program will appear here"
 
-    -- Uncomment this block to pass first stage
-    -- let host = "127.0.0.1"
-    --     port = "4221"
-    --
-    -- BLC.putStrLn $ "Listening on " <> BLC.pack host <> ":" <> BLC.pack port
-    --
-    -- serve (Host host) port $ \(serverSocket, serverAddr) ->
-    --     BLC.putStrLn $ "Accepted connection from " <> BLC.pack (show serverAddr) <> "."
+    let host = "127.0.0.1"
+        port = "4221"
+    BLC.putStrLn $ "Listening on " <> BLC.pack host <> ":" <> BLC.pack port
+    serve (Host host) port $ \(serverSocket, serverAddr) ->
+        BLC.putStrLn $ "Accepted connection from " <> BLC.pack (show serverAddr) <> "."
