815bad16a5ca70b2419087d20fadda6b22565294
Dean Menezes <dean.menezes@fortanix.com>
2023-10-19 09:36:33 -0500

    http 200 OK

--- app/Main.hs
+++ app/Main.hs
@@ -7,11 +7,16 @@ import Network.Simple.TCP
 
 main :: IO ()
 main = do
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
     BLC.putStrLn "Logs from your program will appear here"
-
     let host = "127.0.0.1"
         port = "4221"
     BLC.putStrLn $ "Listening on " <> BLC.pack host <> ":" <> BLC.pack port
-    serve (Host host) port $ \(serverSocket, serverAddr) ->
-        BLC.putStrLn $ "Accepted connection from " <> BLC.pack (show serverAddr) <> "."
+    serve (Host host) port $ \(serverSocket, serverAddr) -> do
+      BLC.putStrLn $ "Accepted connection from " <> BLC.pack (show serverAddr) <> "."
+      res <- recv serverSocket 1024 -- 1024 is the maximum number of bytes to receive
+      case res of
+        Nothing -> BLC.putStrLn "Connection was closed by the client"
+        Just bs ->
+
+
+          send serverSocket "HTTP/1.1 200 OK\r\n\r\n" -- Send a response back to the client
